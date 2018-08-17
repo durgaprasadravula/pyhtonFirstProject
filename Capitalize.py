36e763c092294f872348ebe83901321bad8ef3d8
durgaprasadravula <durgaprasadravula@gmail.com>
2018-08-17 17:40:57 +0100

    capitalise.py

---
+++ Capitalize.py
@@ -0,0 +1,23 @@
+abc_string = 'Welcome to python world'
+
+abc_STRING = 'Python world'
+
+abc_s = "to python world"
+
+a01 = 'welcome to python world'
+
+a02 = 'python world'
+
+a03 = "to python world"
+
+print("a01.capitalize() : ", a01.capitalize(), id(a01), type(a01), len(a01))
+
+print('')
+
+print("a02.capitalize() : ", a02.capitalize(), id(a02), type(a02), len(a02))
+
+print('')
+
+print("a03.capitalize() : ", a03.capitalize(), id(a03), type(a03), len(a03))
+
+print('')
