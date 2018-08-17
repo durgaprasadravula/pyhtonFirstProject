c02b14df6f8f7d576ad524b8d4275881ae046266
durgaprasadravula <durgaprasadravula@gmail.com>
2018-08-17 17:53:19 +0100

    str find rfing methods

---
+++ find_rfind.py
@@ -0,0 +1,22 @@
+#rfind() search backwards in a string
+
+str1 = "Python number string examples"
+
+str2 = "num"
+
+print(str1.find(str2)) #looking for index
+
+print(str1.find(str2, 10))  # start index 10
+
+print(str1.find(str2, 15, 20)) #start and eding index 15 and 20
+
+
+print(str1.rfind(str2)) #looking for index
+
+print(str1.rfind(str2, 10))  # start index 10
+
+print(str1.rfind(str2, 15, 20)) #start and eding index 15 and 20
+
+
+
+
